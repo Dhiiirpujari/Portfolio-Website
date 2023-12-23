6bcc133958b5280f14d9fc1a7ffc42db966ea5a1
Dhiraj Pujari <110563700+Dhiiirpujari@users.noreply.github.com>
2023-12-23 23:24:58 +0530

    Portfolio website

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+1) class="fa fa-comments",class="fa fa-briefcase theese are inbulit classes declared already ypu have to use only 
+for icons 
+
+2) 
+
---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ===================  typing animation ===================== */
+
+var typed = new Typed(".typing", {
+    strings:["Web Developer","Python Developer","Aspiring Data Scientist","Freelancer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
